205ee16404816cdd951d0b0329bc43b9044dc142
towhiid <towhid3008@gmail.com>
2020-10-21 02:56:08 +0600

    few done

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const bodyParser = require('body-parser');
 const express = require('express')
-const app = express()
+const fs = require('fs-extra')
+const cors = require('cors')
+const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oxeo9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors());
+app.use(express.static('services'));
+app.use(fileUpload())
+
 const port = 5000
 
 
@@ -16,12 +28,26 @@ client.connect(err => {
           res.send(result.insertedCount)
       })
   })
-  console.log('db mongo added')
+
+  app.post('/addService', (req, res) => {
+    const file = req.files.file;
+    const name = req.body.name;
+    console.log(name, file);
+    file.mv(`${__dirname}/services/${file.name}`, err => {
+      if(err){
+        console.log(err);
+        return res.status(500).send({msg: 'Failed to upload'});
+      }
+      return res.send({name: file.name, path: `/${file.name}`})
+    })
+  })
+
+  console.log('mongodb added')
 });
 
 
 app.get('/', (req, res) => {
-  res.send('Hello db World!')
+  res.send('Hey mongodb World!')
 })
 
-app.listen(port)
+app.listen(process.env.PORT || port)
